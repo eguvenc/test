18aee3d6ba4fe7484166a9322beb2885bc1edd08
Ersin Güvenç <eguvenc@gmail.com>
2023-08-09 10:25:41 +0300

    Create VueCurrencyInput.vue

---
+++ src/components/VueCurrencyInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-text-field
+    density="compact"
+    :filled="filled"
+    :modelValue="formattedValue"
+    ref="inputRef" 
+  ></v-text-field>
+</template>
+
+<script>
+import { useCurrencyInput } from 'vue-currency-input'
+import { watch } from 'vue';
+
+export default {
+  props: {
+    /**
+     * Type of text input. All HTML type allowed.
+     */
+    type: {
+      type: String,
+      default: "currency",
+    },
+    /**
+     * Use full filled background color style.
+     */
+    filled: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * Currency options
+     */
+    options: Object,
+  },
+  setup (props) {
+    const {
+      inputRef,
+      formattedValue
+    } = useCurrencyInput(props.options)
+    watch(() => props.modelValue, (value) => {
+      setValue(value)
+    })
+    return { inputRef, formattedValue }
+  }
+};
+</script>

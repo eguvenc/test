8f821d3a06fc52e4583059ad4089c8f904e61ec1
Ersin Güvenç <eguvenc@gmail.com>
2023-08-09 10:24:53 +0300

    Create CurrencyInput.vue

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <vue-currency-input 
+    v-bind="commonProps"
+    :options="options"
+  >
+  ></vue-currency-input>
+</template>
+
+<script>
+import Input from "../mixins/input";
+
+export default {
+  mixins: [Input],
+  props: {
+    /**
+     * Currency options
+     */
+    options: Object,
+  },
+};
+</script>
